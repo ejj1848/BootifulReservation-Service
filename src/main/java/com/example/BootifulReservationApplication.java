a46a2da6877cff210432f3b26e8708f83139689f
Eric.Johnson <eric.johnson@astontech.com>
2018-03-12 14:35:24 -0500

    =Update

--- src/main/java/com/example/BootifulReservationApplication.java
+++ src/main/java/com/example/BootifulReservationApplication.java
@@ -1,22 +1,25 @@
 package com.example;
 
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.stereotype.Component;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.Collection;
 import java.util.stream.Stream;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class BootifulReservationApplication {
 
@@ -34,7 +37,18 @@ public class BootifulReservationApplication {
 		SpringApplication.run(BootifulReservationApplication.class, args);
 	}
 }
+@RefreshScope
+@RestController
+class MessageRestController{
 
+    @Value("${message}")
+    private String msg;
+
+    @RequestMapping("/message")
+    String message(){
+        return this.msg;
+    }
+}
 @RepositoryRestResource
 interface ReservationRepository extends JpaRepository<Reservation, Long>{
     @RestResource(path= "by-name")
